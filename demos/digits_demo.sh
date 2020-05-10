b53d411a93a39d18adebf5a0ac41be24f03dbad5
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2020-05-09 18:51:00 -0700

    Whoops, the demo was broken. :(

--- demos/digits_demo.sh
+++ demos/digits_demo.sh
@@ -2,5 +2,5 @@
 # Simple demo script, so I don't have to keep remembering how to use arecord.
 
 MY_PATH="`dirname \"$0\"`"
-arecord -r 16000 -c 1 -f S16_LE | $MY_PATH/../apps/full_reco.app $MY_PATH/speech_model_digits
+arecord -r 16000 -c 1 -f S16_LE | $MY_PATH/../apps/full_reco $MY_PATH/speech_model_digits
 
